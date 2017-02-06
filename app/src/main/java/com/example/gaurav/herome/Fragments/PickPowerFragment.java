61fc35d3a97543e8c873758fca52703f7abd6b6c
Gaurav Sharma <grv7070@gmail.com>
2017-02-06 01:53:11 -0500

    -Finised Pick Power Screen UI -Added Buttons -Buttons are now functional

--- app/src/main/java/com/example/gaurav/herome/Fragments/PickPowerFragment.java
+++ app/src/main/java/com/example/gaurav/herome/Fragments/PickPowerFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.gaurav.herome.R;
 
@@ -18,7 +19,7 @@ import com.example.gaurav.herome.R;
  * Use the {@link PickPowerFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class PickPowerFragment extends Fragment {
+public class PickPowerFragment extends Fragment implements View.OnClickListener {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -27,6 +28,12 @@ public class PickPowerFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private Button turtleBtn;
+    private Button lightningBtn;
+    private Button flightBtn;
+    private Button webSlingingBtn;
+    private Button showBacktoryBtn;
 
     private PickPowerInteractionListener mListener;
 
@@ -64,8 +71,56 @@ public class PickPowerFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_pick_power, container, false);
+        turtleBtn = (Button)view.findViewById(R.id.turtleBtn);
+        lightningBtn = (Button)view.findViewById(R.id.lightningBtn);
+        flightBtn = (Button)view.findViewById(R.id.flightBtn);
+        webSlingingBtn = (Button)view.findViewById(R.id.webSlingBtn);
+        showBacktoryBtn = (Button)view.findViewById(R.id.showBstoryBtn);
+
+        turtleBtn.setOnClickListener(this);
+        lightningBtn.setOnClickListener(this);
+        flightBtn.setOnClickListener(this);
+        webSlingingBtn.setOnClickListener(this);
+
+
+        showBacktoryBtn.setEnabled(false);
+        showBacktoryBtn.getBackground().setAlpha(128);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pick_power, container, false);
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        showBacktoryBtn.setEnabled(true);
+        showBacktoryBtn.getBackground().setAlpha(255);
+
+        turtleBtn.setCompoundDrawablesWithIntrinsicBounds(R.drawable.turtlepower_icon,0,0,0);
+        lightningBtn.setCompoundDrawablesWithIntrinsicBounds(R.drawable.lightning_icon,0,0,0);
+        flightBtn.setCompoundDrawablesWithIntrinsicBounds(R.drawable.rocket_icon,0,0,0);
+        webSlingingBtn.setCompoundDrawablesWithIntrinsicBounds(R.drawable.spiderweb_icon,0,0,0);
+
+        Button btn = (Button)v;
+        int leftDrawable = 0;
+        if(btn == turtleBtn)
+        {
+            leftDrawable = R.drawable.turtlepower_icon;
+        }
+        else if(btn == lightningBtn)
+        {
+            leftDrawable = R.drawable.lightning_icon;
+        }
+        else if(btn == flightBtn)
+        {
+            leftDrawable = R.drawable.rocket_icon;
+        }
+        else if(btn == webSlingingBtn)
+        {
+            leftDrawable = R.drawable.spiderweb_icon;
+        }
+
+        btn.setCompoundDrawablesWithIntrinsicBounds(leftDrawable,0,R.drawable.item_selected_btn,0);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
